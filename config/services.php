b79c7beddb5366c8ec613a251565208f803b32e8
Anton Gribanov <anton.gribanov@gmail.com>
2026-05-06 14:01:37 +0300

    add timeouts

--- config/services.php
+++ config/services.php
@@ -14,6 +14,11 @@ return static function (ContainerConfigurator $container): void {
         ->set('app.redis_dsn', env('MESSENGER_TRANSPORT_DSN'))
         ->set('app.user_job_lock_ttl', env('int:USER_JOB_LOCK_TTL'))
         ->set('app.ytdlp_timeout', env('int:YTDLP_TIMEOUT'))
+        ->set('app.ytdlp_sleep_min_seconds', env('int:YTDLP_SLEEP_MIN_SECONDS'))
+        ->set('app.ytdlp_sleep_max_seconds', env('int:YTDLP_SLEEP_MAX_SECONDS'))
+        ->set('app.ytdlp_http429_retries', env('int:YTDLP_HTTP429_RETRIES'))
+        ->set('app.ytdlp_http429_initial_delay_seconds', env('int:YTDLP_HTTP429_INITIAL_DELAY_SECONDS'))
+        ->set('app.ytdlp_http429_max_delay_seconds', env('int:YTDLP_HTTP429_MAX_DELAY_SECONDS'))
         ->set('app.temp_file_ttl', env('int:TEMP_FILE_TTL'))
         ->set('app.subtitle_temp_dir', env('APP_SUBTITLE_TEMP_DIR'))
         ->set('app.ytdlp_bin', env('YTDLP_BIN'))
@@ -32,6 +37,11 @@ return static function (ContainerConfigurator $container): void {
         ->bind('$subtitleTempDir', '%app.subtitle_temp_dir%')
         ->bind('$ytdlpBin', '%app.ytdlp_bin%')
         ->bind('$processTimeoutSeconds', '%app.ytdlp_timeout%')
+        ->bind('$sleepMinSeconds', '%app.ytdlp_sleep_min_seconds%')
+        ->bind('$sleepMaxSeconds', '%app.ytdlp_sleep_max_seconds%')
+        ->bind('$http429Retries', '%app.ytdlp_http429_retries%')
+        ->bind('$http429InitialDelaySeconds', '%app.ytdlp_http429_initial_delay_seconds%')
+        ->bind('$http429MaxDelaySeconds', '%app.ytdlp_http429_max_delay_seconds%')
         ->bind('$tempFileTtl', '%app.temp_file_ttl%')
         ->bind('$checkRemoteWebhook', '%app.bot_diagnostics_check_remote_webhook%');
 
